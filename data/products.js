5d53bc7fb00cd2c182cfc6da1de6a599cdb5914f
Dinesh <dineshkumar90928@gmail.com>
2024-07-03 16:02:23 +0530

    Convert the product into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,6 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -10,6 +13,29 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetials){
+    this.id = productDetials.id;
+    this.image = productDetials.image;
+    this.name = productDetials.name;
+    this.rating = productDetials.rating;
+    this.priceCents = productDetials.priceCents;
+  }
+
+  getStarUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice(){
+    return `${formatCurrency(this.priceCents)}`
+  }
+}
 
 export const products = [
   {
@@ -670,4 +696,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetials) => {
+  return new Product(productDetials);
+});
--- script/checkout.js
+++ script/checkout.js
@@ -2,7 +2,6 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
-
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
